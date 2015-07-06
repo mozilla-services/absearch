0064f836ea9e9c8382710f4d4eb8678be589ac1d
Tarek Ziade <tarek@ziade.org>
2015-07-06 12:17:35 +0200

    more coverage

---
+++ absearch/tests/test_aws.py
@@ -0,0 +1,17 @@
+from absearch.tests.support import runServers, stopServers, test_config
+from absearch.aws import get_s3_file
+from konfig import Config
+
+
+def setUp():
+    runServers()
+
+
+def tearDown():
+    stopServers()
+
+
+def test_get_s3_file():
+    config = Config(test_config)
+    res = get_s3_file(config['absearch']['config'], config)
+    assert res['interval'] == 31536000
