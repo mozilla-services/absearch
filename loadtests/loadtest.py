46f4495d0d58c040d5d35c0ab544a7d07a96e532
Tarek Ziade <tarek@ziade.org>
2015-12-18 14:32:18 +0100

    replicate the smoke test so we can run it more users

--- loadtests/loadtest.py
+++ loadtests/loadtest.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from gevent import monkey
 monkey.patch_all()
 
@@ -6,20 +7,35 @@ from loads.case import TestCase
 import os
 
 
+LOCALES = [('en-US', 'US', u'Yahoo'),
+           ('en-GB', 'GB', u'Google'),
+           ('de-DE', 'DE', u'Google'),
+           ('pt-BR', 'BR', u'Google'),
+           ('be', 'BE', u'Yandex'),
+           ('ru', 'RU', u'Yandex'),
+           ('uk', 'UK', u'Yandex'),
+           ('kk', 'KK', u'Yandex'),
+           ('tr', 'TR', u'Yandex'),
+           ('zh-TW', 'TW', u'Google'),
+           ('zh-TW', 'HK', u'Google'),
+           ('fr-FR', 'FR', u'Google'),
+           ('zh-CN', 'CN', u'百度')]
+
+
+
 class TestSearch(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestSearch, self).__init__(*args, **kwargs)
 
     def test_api(self):
-        choices = (('en-US', 'US'),
-                   ('fr-FR', 'FR'),
-                   ('cs-CZ', 'CZ'))
 
         # getting a cohort
-        locale, territory = random.choice(choices)
+        locale, territory, expected = random.choice(LOCALES)
         res = self.session.get(self.server_url +
-                             '/1/firefox/39/beta/%s/%s/release/default/default'
+                             '/1/firefox/43/beta/%s/%s/release/default/default'
                               % (locale, territory))
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue('settings' in res.json())
+        self.assertTrue('cohort' not in res.json())
+        self.assertEqual(res.json()['settings']['searchDefault'], expected)
