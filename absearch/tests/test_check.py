c2483a913757e137ef4dd53a8ddfc0b26525663f
Tarek Ziade <tarek@ziade.org>
2016-03-22 10:18:28 +0100

    added the ability to control array of values (for errors like case sensitivity)

--- absearch/tests/test_check.py
+++ absearch/tests/test_check.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+
+from jsonschema.exceptions import ValidationError
 from absearch.tests.support import runServers, stopServers, capture
 from absearch.check import main
 
@@ -37,3 +40,23 @@ def test_check_fails():
 
     # we should fail
     assert res == 1
+
+
+def test_check_enum_fails():
+    conf = os.path.join(os.path.dirname(__file__), '..', '..', 'data',
+                        'config.json')
+    os.rename(conf, conf + '.saved')
+    broken_conf = os.path.join(os.path.dirname(__file__), 'broken-conf.json')
+    e = None
+    try:
+        shutil.copy(broken_conf, conf)
+        with capture():
+            main([])
+    except ValidationError as e:
+        # that's what we want
+        pass
+    finally:
+        os.rename(conf + '.saved', conf)
+
+    # we should fail
+    assert e is not None
