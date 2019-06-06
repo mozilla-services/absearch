0ba054bf7e1c1fbfac0777e8352b2232655e2564
Benson Wong <mostlygeek+git@gmail.com>
2019-06-06 09:31:01 -0700

    Remove retry from scripts/deploy-dockerhub.sh
    Asked Adrian where the code came from.  We don't remember.  It's
    probably not necessary anymore so getting rid of the retry for docker
    login

--- scripts/deploy-dockerhub.sh
+++ scripts/deploy-dockerhub.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-# THIS IS MEANT TO BE RUN BY CI
-
 set -e
 
-# Usage: retry MAX CMD...
-# Retry CMD up to MAX times. If it fails MAX times, returns failure.
-# Example: retry 3 docker push "$DOCKERHUB_REPO:$TAG"
-function retry() {
-    max=$1
-    shift
-    count=1
-    until "$@"; do
-        count=$((count + 1))
-        if [[ $count -gt $max ]]; then
-            return 1
-        fi
-        echo "$count / $max"
-    done
-    return 0
-}
-
 # configure docker creds
-retry 3  echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
+echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
 
 # docker tag and push git branch to dockerhub
 if [ -n "$1" ]; then
